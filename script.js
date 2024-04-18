86a5d70f9db19171d6fe7df87a5b3335b4180390
Vacaminando <167458950+Vacaminando@users.noreply.github.com>
2024-04-18 16:05:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    
-    envelope.click( function() {
+    var words = $(".words"); // Selecciona el elemento que contiene el texto
+
+    envelope.click(function() {
         open();
     });
-    btn_open.click( function() {
+    btn_open.click(function() {
         open();
+        showText(); // Llama a la función showText() cuando se hace clic en el botón "Open"
     });
-    btn_reset.click( function() {
+    btn_reset.click(function() {
         close();
+        hideText(); // Llama a la función hideText() cuando se hace clic en el botón "Reset"
     });
 
     function open() {
-        envelope.addClass("open")
-           .removeClass("close");
+        envelope.addClass("open").removeClass("close");
     }
     function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-    }
-   
+        envelope.addClass("close").removeClass("open");
+    }
+    function showText() {
+        words.show(); // Muestra el texto
+    }
+    function hideText() {
+        words.hide(); // Oculta el texto
+    }
 });
